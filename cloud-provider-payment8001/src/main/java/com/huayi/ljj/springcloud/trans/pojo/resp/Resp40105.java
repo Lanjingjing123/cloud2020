e63935a06edf8ff8565d72b6c3fdb5565a607b54
lanjingjing <1792539263@qq.com>
2020-06-28 17:54:46 +0800

    新增40105交易配置

--- cloud-provider-payment8001/src/main/java/com/huayi/ljj/springcloud/trans/pojo/resp/Resp40105.java
+++ cloud-provider-payment8001/src/main/java/com/huayi/ljj/springcloud/trans/pojo/resp/Resp40105.java
@@ -1,12 +1,18 @@
 package com.huayi.ljj.springcloud.trans.pojo.resp;
 
 import com.huayi.ljj.springcloud.pojo.resp.BaseResp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author lanjingjing
  * @description 产品种类配置
  * @date 2020/6/27
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Resp40105 extends BaseResp {
     private static final long serialVersionUID = -5859567475401123533L;
     /**
